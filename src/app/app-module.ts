dbde6b4e637837558f93a571c57eed2c76e1a039
Alaa Ahmed <alaaahmedabdelhalim@gmail.com>
2025-11-09 21:15:40 +0200

    Setup Environment & Make Home Section

---
+++ src/app/app-module.ts
@@ -0,0 +1,28 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Header } from './Components/header/header';
+import { Footer } from './Components/footer/footer';
+import { About } from './Components/about/about';
+
+@NgModule({
+  declarations: [
+    App,
+    Header,
+    Footer,
+ 
+    About
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
