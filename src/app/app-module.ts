ef430d639cd2c3227d1bf85c75cb186afa542389
Alaa Ahmed <alaaahmedabdelhalim@gmail.com>
2025-11-09 23:04:07 +0200

    Responsive Designs

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,14 +5,13 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Header } from './Components/header/header';
 import { Footer } from './Components/footer/footer';
-import { About } from './Components/about/about';
+import { About } from './Components/About/about';
 
 @NgModule({
   declarations: [
     App,
     Header,
     Footer,
- 
     About
   ],
   imports: [
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
-import { Component, signal } from '@angular/core';
-
+import { Component, OnInit, signal } from '@angular/core';
+import * as AOS from 'aos'; 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+ ngOnInit(): void {
+  AOS.init({
+    duration: 800, // مدة الحركة بالمللي ثانية
+    once: false,   // true = تتحرك مرة واحدة فقط
+  });
+}
+
   protected readonly title = signal('Party');
 }
