43af6c0921e4d58b5ef05d8e7cc9083016a2b3d7
AlaaAhmedAbdalhalim <alaaahmedabdalhalim@gmail.com>
2025-11-17 14:13:53 +0200

    Member Section

--- src/app/Components/members/members.ts
+++ src/app/Components/members/members.ts
@@ -7,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrl: './members.css',
 })
 export class Members {
+members = [
+  {
+    image: 'assets/Members/AhmedGhareeb.jpeg',
+    name: 'أحمد غريب',
+    role: 'رئيس الحزب'
+  },
+  {
+    image: 'assets/Members/Hassan.jpeg',
+    name: 'القبطان حسان',
+    role: 'أمين التنظيم'
+  },
+  {
+    image: 'assets/Members/MariamMohamed.jpeg',
+    name: 'مريم محمد',
+    role: 'عضو هيئة مكتب'
+  },
+  {
+    image: 'assets/Members/MomenSamuel.jpeg',
+    name: 'مؤمن صموئيل',
+    role: 'عضو هيئة مكتب'
+  },
+  {
+    image: 'assets/Members/Mamdouh.jpeg',
+    name: 'محمد ممدوح',
+    role: 'عضو هيئة مكتب'
+  },
+  {
+    image: 'assets/Members/Salah.jpeg',
+    name: 'صلاح عثمان',
+    role: 'عضو هيئة مكتب'
+  },
+   {
+    image: 'assets/Members/Sanjar.jpeg',
+    name: 'جمال سنجر',
+    role: 'عضو هيئة مكتبأمين الطاقة  والثروة المعدنية'
+  },
+];
 
 }
