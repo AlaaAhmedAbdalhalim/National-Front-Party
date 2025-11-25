0e5ee4fdb8f9247621b746f31ea140b7855b43dc
Alaa Ahmed <alaaahmedabdelhalim@gmail.com>
2025-11-25 22:53:05 +0200

    Join Us Section

--- src/app/Components/join-us/join-us.ts
+++ src/app/Components/join-us/join-us.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-join-us',
   standalone: false,
@@ -8,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class JoinUs {
 
+  currentStep = 0;
+  totalSteps = 4;
+
+  get progress() {
+    return ((this.currentStep + 1) / this.totalSteps) * 100;
+  }
+
+  nextStep() {
+    if (this.currentStep < this.totalSteps - 1) {
+      this.currentStep++;
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }
+
+  prevStep() {
+    if (this.currentStep > 0) {
+      this.currentStep--;
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }
+
+  submitForm() {
+    alert("تم إرسال الطلب بنجاح");
+  }
 }
+
