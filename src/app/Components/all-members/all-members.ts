8c3749f9cdefdadfb69a66647a8cf1a5f3245613
AlaaAhmedAbdalhalim <alaaahmedabdalhalim@gmail.com>
2025-12-23 12:46:50 +0200

    Edit ALl Members Component For Admin View

--- src/app/Components/all-members/all-members.ts
+++ src/app/Components/all-members/all-members.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MembersList, MembersService } from '../../Services/members-service';
+import { AuthService } from '../../Services/auth-service';
 
 @Component({
   selector: 'app-all-members',
@@ -9,16 +10,32 @@ import { MembersList, MembersService } from '../../Services/members-service';
 })
 export class AllMembers {
   membersList: MembersList[] = [];  
+  isAdmin: boolean = false;
 
-  constructor(private membersService: MembersService) {}
+  constructor(private membersService: MembersService, private authService: AuthService) {}
 
   ngOnInit(): void {
+        this.checkIfAdmin();
     this.loadMembers();
   }
     encodeImage(image: string): string {
     return encodeURIComponent(image);
   }
+ checkIfAdmin() {
+const token = this.authService.getToken();
+    if (!token) {
+      this.isAdmin = false;
+      return;
+    }
 
+    try {
+      const payload = token.split('.')[1];
+      const decoded = JSON.parse(atob(payload));
+      this.isAdmin = decoded.role === 'admin';
+    } catch (e) {
+      this.isAdmin = false;
+    }
+  }
   loadMembers() {
     this.membersService. getMembers().subscribe({
       next: (data) => {
@@ -32,4 +49,17 @@ export class AllMembers {
   }
     
 
+  // 🗑️ DELETE
+  deleteMember(id: number) {
+    if (!confirm('هل أنت متأكد من الحذف؟')) return;
+
+    this.membersService.deleteMember(id).subscribe({
+      next: () => {
+        this.membersList = this.membersList.filter(m => m.id !== id);
+      },
+      error: (err) => {
+        console.error('Delete error', err);
+      }
+    });
+  }
 }
