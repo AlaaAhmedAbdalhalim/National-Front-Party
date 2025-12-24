f7e8255fbb8c8804062ceab53bb7846fb7234261
Alaa Ahmed <alaaahmedabdelhalim@gmail.com>
2025-12-24 22:55:57 +0200

    Edit All Message Component

--- src/app/Components/all-messages/all-messages.ts
+++ src/app/Components/all-messages/all-messages.ts
@@ -12,4 +12,23 @@ export class AllMessages {
   selectedType: string = '';
   messages: MessagesList[] = [];
   
+    constructor(private message: Messages ) {}
+  
+    ngOnInit(): void {
+      
+      this.loadMessages();
+    }
+  
+    loadMessages() {
+      this.message.getMessages().subscribe({
+        next: (data) => {
+          this.messages = data;
+          console.log(this.messages);
+        },
+        error: (err) => {
+          console.error('Error loading messages', err);
+        }
+      });
+    }
+    
 }
